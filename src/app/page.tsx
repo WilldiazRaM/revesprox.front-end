ea6723d799a332366b15957c8cf004770c81582b
WilldiazRaM <willdiazsantander@gmail.com>
2025-06-07 17:48:27 -0400

    Primer commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        ¡Hola desde Next.js + Tailwind!
+      </h1>
+    </main>
+  );
+}
