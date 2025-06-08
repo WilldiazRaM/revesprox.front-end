d8eaac6f00571ff4b9b3cf3e22503a763cb0a816
WilldiazRaM <willdiazsantander@gmail.com>
2025-06-07 22:26:50 -0400

    mod page.tsx test

---
+++ src/app/cotizar/page.tsx
@@ -0,0 +1,8 @@
+export default function CotizarPage() {
+  return (
+    <main>
+      <h1>Página de Cotización</h1>
+      {/* Aquí puedes importar y usar tu componente CotizacionForm */}
+    </main>
+  )
+}
---
+++ src/app/galeria/page.tsx
@@ -0,0 +1,8 @@
+export default function GaleriaPage() {
+  return (
+    <main>
+      <h1>Página de Galeria</h1>
+      {/* Aquí puedes importar y usar tu componente CotizacionForm */}
+    </main>
+  )
+}
